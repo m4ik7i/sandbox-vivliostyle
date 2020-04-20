118d80fb7f4e50a6158ac8fd7d31c3b555b694a3
m4ik7i <m4ik7i@gmail.com>
2020-04-20 23:01:04 +0900

    feat: build:pandoc

---
+++ scripts/build_pandoc.js
@@ -0,0 +1,42 @@
+const { spawn } = require('child-process-promise');
+const shell = require('shelljs');
+
+if (!shell.which('pandoc')) {
+  console.error(new Error('pandoc has to be installed'));
+  process.exit(1);
+}
+
+const source = process.argv[2];
+
+if (!source) {
+  console.error(new Error('source file is not defined'));
+  process.exit(1);
+}
+
+(async () => {
+  try {
+    await (() => {
+      const promise = spawn('pandoc', ['--standalone', '--output', 'build/output.html', source]);
+      promise.childProcess.stdout.on('data', (data) => {
+        process.stdout.write(String(data));
+      });
+      return promise;
+    })();
+    await (() => {
+      const promise = spawn('./node_modules/.bin/vivliostyle', [
+        'build',
+        '--book',
+        '--output',
+        'build/output.pdf',
+        'build/output.html',
+      ]);
+      promise.childProcess.stdout.on('data', (data) => {
+        process.stdout.write(String(data));
+      });
+      return promise;
+    })();
+  } catch (err) {
+    console.error(err);
+    process.exit(err.code);
+  }
+})();
